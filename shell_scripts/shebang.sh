ce3f7c69aadd1e545cb52ed749e9608be33b4d38
JonanaBanana <jonathan@majj.dk>
2026-02-11 13:31:46 +0100

    update

--- shell_scripts/shebang.sh
+++ shell_scripts/shebang.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+#this shell file is an installer for all dependencies of the Thermal Vision Navigation package and research environment. 
+#It is intended to be run a clean installation of Ubuntu 22.04 LTS, and will install Ceres Solver 2.2.0, Pangolin, OpenCV, and Kindr, as well as all necessary dependencies for these packages.
+#This will allow for easy installation and benchmarking of SOTA visual inertial navigation algorithms such as VINS-Multi, ORB-SLAM3 and ROVTIO in a ROS2 environment.
+
 cd ~
 wait
 
@@ -145,14 +150,6 @@ cmake -B build
 
 cmake --build build
 
-#cd build
-
-#make -j16
-
-#sudo make install
-
-#sudo ldconfig
-
 ## install OpenCV with OpenCV_contrib
 cd ~/thirdparty/
 wait
@@ -206,64 +203,4 @@ wait
 
 sudo make install -j $(nproc)
 
-
 cd ~
-
-locale  # check for UTF-8
-
-sudo apt update
-wait
-
-sudo apt install locales -y
-wait
-
-sudo locale-gen en_US en_US.UTF-8
-wait
-
-sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
-wait
-
-export LANG=en_US.UTF-8
-wait
-
-locale  # verify settings
-wait
-
-sudo apt install software-properties-common -y
-wait
-
-sudo add-apt-repository universe
-wait
-
-sudo apt update
-wait
-
-sudo apt install curl -y
-wait
-
-export ROS_APT_SOURCE_VERSION=$(curl -s https://api.github.com/repos/ros-infrastructure/ros-apt-source/releases/latest | grep -F "tag_name" | awk -F\" '{print $4}')
-wait
-
-curl -L -o /tmp/ros2-apt-source.deb "https://github.com/ros-infrastructure/ros-apt-source/releases/download/${ROS_APT_SOURCE_VERSION}/ros2-apt-source_${ROS_APT_SOURCE_VERSION}.$(. /etc/os-release && echo ${UBUNTU_CODENAME:-${VERSION_CODENAME}})_all.deb"
-wait
-
-sudo dpkg -i /tmp/ros2-apt-source.deb
-wait
-
-sudo apt update
-wait
-
-sudo apt upgrade -y
-wait
-
-sudo apt install ros-humble-desktop -y
-wait
-
-source /opt/ros/humble/setup.bash
-wait
-
-echo "source /opt/ros/humble/setup.bash" >> ~/.bashrc
-wait
-
-sudo apt install python3-colcon-common-extensions -y
-wait
