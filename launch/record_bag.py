2b7deb3d4e497f177f171bb7b2e5a681a5304c77
Jonathan Nielsen <jonathan@majj.dk>
2026-03-06 10:05:41 +0100

    added rosbag recorder

---
+++ launch/record_bag.py
@@ -0,0 +1,36 @@
+from launch import LaunchDescription
+from launch.actions import ExecuteProcess, DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+import os
+from datetime import datetime
+
+def generate_launch_description():
+
+    # Timestamped output folder
+    bag_name = 'voxl_bag_' + datetime.now().strftime('%Y%m%d_%H%M%S')
+    output_dir = os.path.join('/bagfiles', bag_name)
+
+    topics = [
+        '/ircam',
+        '/low_light_down_gray',
+        '/tracking_down',
+        '/tracking_front',
+        '/hires_front_small_grey',
+        '/imu_apps',
+        '/fmu/out/vehicle_gps_position',
+        '/fmu/out/vehicle_local_position',
+        '/fmu/out/vehicle_odometry'
+    ]
+
+    return LaunchDescription([
+        ExecuteProcess(
+            cmd=[
+                'ros2', 'bag', 'record',
+                '--output', output_dir,
+                '--storage', 'mcap',
+                '--storage-preset-profile', 'zstd_fast'
+            ] + topics,
+            output='screen',
+            shell=False,
+        )
+    ])
