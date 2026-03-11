1efd02ba1fe497bceb0e820f5a4653410bf756b7
Jonathan Nielsen <jonathan@majj.dk>
2026-03-11 14:05:37 +0100

    added encoded decoded record launch

---
+++ launch/record_bag_misp_encoded_launch.py
@@ -0,0 +1,33 @@
+from launch import LaunchDescription
+from launch.actions import ExecuteProcess, DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+import os
+from datetime import datetime
+
+def generate_launch_description():
+
+    # Timestamped output folder
+    bag_name = 'voxlbag_encoded_' + datetime.now().strftime('%Y%m%d_%H%M%S')
+    output_dir = os.path.join('/bagfiles', bag_name)
+
+    topics = [
+        '/low_light_down_misp_encoded',
+        '/tracking_down_misp_encoded',
+        '/tracking_front_misp_encoded',
+        '/imu_apps',
+        '/fmu/out/vehicle_gps_position',
+        '/fmu/out/vehicle_local_position',
+        '/fmu/out/vehicle_odometry'
+    ]
+
+    return LaunchDescription([
+        ExecuteProcess(
+            cmd=[
+                'ros2', 'bag', 'record',
+                '--output', output_dir,
+                '--max-bag-size', '500000000',   # 5GB bag size (splits bag when exceeding)
+            ] + topics,
+            output='screen',
+            shell=False,
+        )
+    ])
