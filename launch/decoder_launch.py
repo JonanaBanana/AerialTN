e122e2a85c45fd2b440dfd9e0fd48d03e65f2cba
Jonathan Nielsen <jonathan@majj.dk>
2026-03-23 11:40:43 +0100

    moved utils to AerialTN_utility

--- launch/decoder_launch.py
+++ launch/decoder_launch.py
@@ -1,8 +1,12 @@
+from ament_index_python import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
+import os
 
 
 def generate_launch_description():
+    pkg_dir = get_package_share_directory('aerial_tn')
+
     return LaunchDescription([
         Node(
             package='aerial_tn',
@@ -14,7 +18,7 @@ def generate_launch_description():
                 'frame_id': 'low_light_down',
                 'live_stream': False,   #enable only when streaming over wifi, 
                                         #as it will provide additional checks to increase stream stability
-                'convert_to_bgr': True #enable only when you want to visualize in rviz. 
+                'convert_to_bgr': False #enable only when you want to visualize in rviz. 
                                         #enabling doubles computation per frame, but allows bgr8 output instead of yuv420p.
             }]
             ),
@@ -27,7 +31,7 @@ def generate_launch_description():
                 'output_topic': '/tracking_down_misp_decoded',
                 'frame_id': 'tracking_down',
                 'live_stream': False, 
-                'convert_to_bgr': True
+                'convert_to_bgr': False
             }]
             ),
         Node(
@@ -39,7 +43,7 @@ def generate_launch_description():
                 'output_topic': '/tracking_front_misp_decoded',
                 'frame_id': 'tracking_front',
                 'live_stream': False, 
-                'convert_to_bgr': True
+                'convert_to_bgr': False
             }]
             ),
         Node(
