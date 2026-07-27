02c14560abddceb4cc57c7f936ef310245f2596a
Jonathan Nielsen <jonathan@majj.dk>
2026-07-27 15:48:33 +0200

    updated lightware

--- launch/sf30d_launch.py
+++ launch/sf30d_launch.py
@@ -4,12 +4,13 @@ from launch_ros.actions import Node
 def generate_launch_description():
     return LaunchDescription([
         Node(
-            package='lightwarelidar2',
+            package='lightwarelidar2-sf30d',
             executable='sf30d',
             name='sf30d',
             parameters=[{
                 'port': '/dev/ttyACM0',
                 'frame_id': 'sf30d',
+                'smoothing_window': 5,
             }],
             output='screen',
         )
